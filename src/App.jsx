afb32cf51d179a35de528db1dcf8593923fe6147
Victor Hordones Ramos <victorhordonesramos38@gmail.com>
2026-01-14 20:51:44 -0300

    feat: badges

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
             id: 1,
             titulo: 'Aula React 01',
             categoria: 'Educação',
-            nivel: 'Fácil',
-            status: 'concluido',
+            nivel: 'Alto',
+            status: false,
             descricao:
                 '1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ex doloribus quaerat est nemo veniam esse illo labore, porro aliquid nihil sed corporis alias laborum assumenda, blanditiis qui incidunt voluptatibus.',
         },
@@ -18,7 +18,7 @@ function App() {
             id: 2,
             titulo: 'Aula Natação 01',
             categoria: 'Esporte',
-            nivel: 'Médio',
+            nivel: 'Baixo',
             status: true,
             descricao:
                 '2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ex doloribus quaerat est nemo veniam esse illo labore, porro aliquid nihil sed corporis alias laborum assumenda, blanditiis qui incidunt voluptatibus.',
@@ -28,7 +28,7 @@ function App() {
             id: 3,
             titulo: 'Agachar com 500kg',
             categoria: 'Esporte',
-            nivel: 'Difícil',
+            nivel: 'Alto',
             status: true,
             descricao:
                 '3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ex doloribus quaerat est nemo veniam esse illo labore, porro aliquid nihil sed corporis alias laborum assumenda, blanditiis qui incidunt voluptatibus.',
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,10 @@
-import { Check } from "lucide-react";
+import { Badge, BadgeAlert, BadgeCheck, Check } from "lucide-react";
 import "./Task.css"
 
 const Task = ({titulo, categoria, nivel, status}) => {
     return (
         <>
 
-            
-        
         <div className="dashboard">
             <div className="task-card">
                 <h2 className="task-title">{titulo}</h2>
@@ -19,20 +17,21 @@ const Task = ({titulo, categoria, nivel, status}) => {
                     </span>
                     <span className="meta-item">
                         <span className="status completed">
-
-                            {status && <Check />}
-
-
-
+                            {status ? <BadgeCheck /> : <Badge />}
                         </span>
                     </span>
+                    {nivel == 'Alto' && 
+                    <span className="meta-item">
+                        <span className="status completed">
+                            <BadgeAlert />
+                        </span>
+                    </span>
+                    }
                 </div>
                 {/* <p className="task-description">Set up secure user authentication with JWT tokens and password hashing.</p>
                 <button className="action-button">View Implementation Details</button> */}
             </div>
         </div>
-
-
         </>
     );
 }
